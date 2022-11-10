d87a3f33642d37519af6ae57119fe35f0ebefb6a
Renata Porto Gransoti <renata.gransoti@edu.unifil.br>
2022-11-10 16:44:38 -0300

    Starting to implement Board and Piece

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -3,10 +3,12 @@ package boardgame;
 public class Position {
 	 private int row;
 	 private int column;
+//	 private Piece[][] pieces;
 	 
 	public Position(int row, int column) {
 		this.row = row;
 		this.column = column;
+	//	pieces = new Piece[rows][columns];
 	}
 
 	public int getRow() {
@@ -24,7 +26,11 @@ public class Position {
 	public void setColumns(int column) {
 		this.column = column;
 	}
-	
+//	
+//	public Piece piece(int row, int columns) {
+//		this.columns[row][column];
+//	}
+//	
 	@Override
 	public String toString() {
 		return row + ", " + column;
